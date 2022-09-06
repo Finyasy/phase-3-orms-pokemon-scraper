9a58aa57841f16ee99ef7256efe4e130ddc915ca
Bryan Bosire <bryanbosire@gmail.com>
2022-09-06 05:30:24 +0300

    commit tree 2dd667990ee6f3f9dcd45a5ef60cdbc025eda92c

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,22 @@
 class Pokemon
+    # schema_migration.sql creates pokemon table
+    attr_accessor :id, :name, :type, :db
+    def initialize(id=nil, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    # creates pokemon table (FAILED
+    def self.create_table
+        db = SQLite3::Database.new("pokemon.db")
+        db.execute(<<-SQL)
+            CREATE TABLE IF NOT EXISTS pokemon (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                type TEXT
+            )
+        SQL
+    end
 end
